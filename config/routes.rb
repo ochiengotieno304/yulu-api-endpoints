620f3d39e07c9b9eb2842a900b42572ffef62333
Ochieng Otieno <ochiengotien071@gmail.com>
2022-07-27 22:53:01 +0300

    fixed login token mismatch

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
   resources :users, only: [:create]
   post "/login", to: "users#login"
   get "/auto_login", to: "users#auto_login"
